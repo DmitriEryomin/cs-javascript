f41b450232e2139fd39e932c6095940ad458c4f6
Dmitri Eryomin <dmitri.eryomin@gmail.com>
2023-01-24 20:29:21 +0400

    feat: added addToHead and addToTail for DoublyLinkedList

--- data-structures/DoublyLinkedList.js
+++ data-structures/DoublyLinkedList.js
@@ -1,12 +1,6 @@
 const BidirectionalNode = require('./BidirectionalNode');
 const LinkedList = require('./LinkedList');
 
-class Test {
-  test() {
-    console.log('test');
-  }
-}
-
 class DoublyLinkedList {
   /** @type {(BidirectionalNode | null)} */
   #tail;
@@ -20,15 +14,47 @@ class DoublyLinkedList {
   }
 
   addToHead(data) {
+    const newHead = new BidirectionalNode(data);
     if (this.#head === null) {
-      this.#head = new BidirectionalNode(data);
+      this.#head = newHead;
+      this.#tail = newHead;
       return;
     }
 
-    const newHead = new BidirectionalNode(data);
-    const oldHead = this.#head;
-    newHead.setNextNode(oldHead);
-    oldHead.setPreviousNode(newHead);
+    const currentHead = this.#head;
+    newHead.setNextNode(currentHead);
+    currentHead.setPreviousNode(newHead);
     this.#head = newHead;
   }
+
+  addToTail(data) {
+    const newTail = new BidirectionalNode(data);
+    if (this.#tail === null) {
+      this.#tail = newTail;
+      this.#head = newTail;
+      return;
+    }
+
+    const currentTail = this.#tail;
+    currentTail.setNextNode(newTail);
+    newTail.setPreviousNode(currentTail);
+    this.#tail = newTail;
+  }
+
+  get head() {
+    return this.#head.data;
+  }
+
+  get tail() {
+    return this.#tail.data;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.addToTail(2);
+list.addToTail(6);
+console.log('head', list.head);
+console.log('tail', list.tail);
+list.addToTail(4);
+console.log('head', list.head);
+console.log('tail', list.tail);
